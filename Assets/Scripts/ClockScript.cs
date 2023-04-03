04c897b73141336b3bdab3ffe0924cbe19255fe6
nfrande <niklas.frande@gmail.com>
2023-04-03 10:11:32 +0300

    Second version with smooth movement

--- Assets/Scripts/ClockScript.cs
+++ Assets/Scripts/ClockScript.cs
@@ -25,11 +25,11 @@ public class ClockScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      var time = DateTime.Now;
-           hoursPivot.localRotation = Quaternion.Euler(0f,hoursToDegrees * time.Hour, 0f); 
+      TimeSpan time = DateTime.Now.TimeOfDay;
+           hoursPivot.localRotation = Quaternion.Euler(0f,hoursToDegrees * (float) time.TotalHours, 0f); 
             minutesPivot.localRotation =
-			Quaternion.Euler(0f, minutesToDegrees * time.Minute, 0f);
+			Quaternion.Euler(0f, minutesToDegrees * (float) time.TotalMinutes, 0f);
 		secondsPivot.localRotation =
-			Quaternion.Euler(0f, secondsToDegrees * time.Second, 0f);
+			Quaternion.Euler(0f, secondsToDegrees * (float) time.TotalSeconds, 0f);
     }
 }
